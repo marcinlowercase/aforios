b5e5c1227f84bb55895943a4b0d7e7a1ed576e62
Theo <marcinlowercase@gmail.com>
2025-11-12 13:54:53 -0600

    Initial Commit

---
+++ a/aApp.swift
@@ -0,0 +1,17 @@
+//
+//  aApp.swift
+//  a
+//
+//  Created by Tom on 11/12/25.
+//
+
+import SwiftUI
+
+@main
+struct aApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
